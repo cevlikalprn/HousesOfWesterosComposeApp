905c4dbbdd8a39975d664ab46715a142ef5fd34a
Alperen <devcalprn@gmail.com>
2021-09-02 15:20:55 +0300

    Data Class and constants Added

---
+++ app/src/main/java/com/cevlikalprn/housesofwesteros/Constants.kt
@@ -0,0 +1,65 @@
+package com.cevlikalprn.housesofwesteros
+
+import java.util.ArrayList
+
+object Constants {
+
+    val houseList: ArrayList<HouesesOfWesteros> = arrayListOf(
+
+        HouesesOfWesteros(
+            houseId = 0,
+            houseName = "Stark",
+            houseWords = "Winter is Coming",
+            housePicture = "https://cdn.icon-icons.com/icons2/1819/PNG/512/stark_115482.png"
+        ),
+        HouesesOfWesteros(
+            houseId = 1,
+            houseName = "Arryn",
+            houseWords = "Winter is Coming",
+            housePicture = "https://cdn.icon-icons.com/icons2/1819/PNG/512/stark_115482.png"
+        ),
+        HouesesOfWesteros(
+            houseId = 2,
+            houseName = "Baratheon",
+            houseWords = "Winter is Coming",
+            housePicture = "https://cdn.icon-icons.com/icons2/1819/PNG/512/stark_115482.png"
+        ),
+        HouesesOfWesteros(
+            houseId = 3,
+            houseName = "Tully",
+            houseWords = "Winter is Coming",
+            housePicture = "https://cdn.icon-icons.com/icons2/1819/PNG/512/stark_115482.png"
+        ),
+        HouesesOfWesteros(
+            houseId = 4,
+            houseName = "Greyjoy",
+            houseWords = "Winter is Coming",
+            housePicture = "https://cdn.icon-icons.com/icons2/1819/PNG/512/stark_115482.png"
+        ),
+        HouesesOfWesteros(
+            houseId = 5,
+            houseName = "Lannister",
+            houseWords = "Winter is Coming",
+            housePicture = "https://cdn.icon-icons.com/icons2/1819/PNG/512/stark_115482.png"
+        ),
+        HouesesOfWesteros(
+            houseId = 6,
+            houseName = "Tyrell",
+            houseWords = "Winter is Coming",
+            housePicture = "https://cdn.icon-icons.com/icons2/1819/PNG/512/stark_115482.png"
+        ),HouesesOfWesteros(
+            houseId = 7,
+            houseName = "Martell",
+            houseWords = "Winter is Coming",
+            housePicture = "https://cdn.icon-icons.com/icons2/1819/PNG/512/stark_115482.png"
+        ),
+        HouesesOfWesteros(
+            houseId = 8,
+            houseName = " Targaryen",
+            houseWords = "Winter is Coming",
+            housePicture = "https://cdn.icon-icons.com/icons2/1819/PNG/512/stark_115482.png"
+        )
+
+    )
+
+}
---
+++ app/src/main/java/com/cevlikalprn/housesofwesteros/HouesesOfWesteros.kt
@@ -0,0 +1,15 @@
+package com.cevlikalprn.housesofwesteros
+
+import java.util.ArrayList
+
+data class HouesesOfWesteros(
+    val houseId: Int,
+    val houseName: String,
+    val houseWords: String,
+    val housePicture: String
+)
+
+
+
+
+
